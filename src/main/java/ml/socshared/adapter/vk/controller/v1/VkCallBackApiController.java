4fa2fee5ed5de713a82291611f5b6dbc984e6971
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-16 22:52:16 +0300

    Пофиксил баги и исправил конфиг

--- src/main/java/ml/socshared/adapter/vk/controller/v1/VkCallBackApiController.java
+++ src/main/java/ml/socshared/adapter/vk/controller/v1/VkCallBackApiController.java
@@ -2,15 +2,13 @@ package ml.socshared.adapter.vk.controller.v1;
 
 import lombok.extern.slf4j.Slf4j;
 import ml.socshared.adapter.vk.api.v1.rest.VkCallBackApi;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.UUID;
 
 @RequestMapping("api/v1/")
+@RestController
 @Slf4j
 public class VkCallBackApiController implements VkCallBackApi {
 
